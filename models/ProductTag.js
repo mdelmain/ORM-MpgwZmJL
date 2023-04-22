852ef01db96f6a9f8962b4199b2a208adc23af76
Mandy Delmaine <mandydelmaine@gmail.com>
2023-04-22 16:51:22 -0400

    updated database models for category, product, producttag and tag

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,28 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        // References the `product` model's id
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        // References the `tag` model's id
+        model: 'tag',
+        key: 'id',
+      },
+    }
   },
   {
     sequelize,
